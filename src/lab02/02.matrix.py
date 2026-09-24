312afc412b6795e833c127d2cf30244a8656a877
kalpak79 <egorkolpak08@gmail.com>
2026-09-24 22:08:44 +0300

    02

--- src/lab02/02.matrix.py
+++ src/lab02/02.matrix.py
@@ -1,10 +1,57 @@
-def transpose(mat: list[list[float | int]]):
-    an = [[] for _ in range(len(mat))]
-    k = 0
-    for i in range(len(mat)):
-        for j in range(len(mat[i])):
-            an[k].append(mat[j][i])
-        k+=1
-    return an
+from ..lib import testing
 
-print(transpose([[1,2,3],[4,5,6],[7,8,9]]))
+def rect(mat: list[list[int | float]]): # функция для проверки прямоугольности матрицы
+    if len(mat) == 0:
+            return []
+         
+    row_len = len(mat[0])
+    for row in mat:
+        if len(row) != row_len:
+            raise ValueError('Рваная матрица')
+
+def transpose(mat: list[list[int | float]]) -> list[list]:
+    rect(mat)
+    # Транспонируем с помощью zip(*mat).
+    # Функция zip возвращает кортежы, преобразуем listом.
+    return [list(x) for x in zip(*mat)]
+
+def row_sums(mat: list[list[float | int]]):
+
+    rect(mat)
+
+    return [sum(x) for x in mat]
+
+def col_sums(mat: list[list[float | int]]):
+    rect(mat)
+
+    return [sum(x) for x in transpose(mat)]
+
+test_transpose=[
+    [[1, 2, 3]],
+    [[1], [2], [3]],
+    [[1, 2], [3, 4]],
+    [],
+    [[1, 2], [3]]
+]
+
+test_row_sums=[
+    [[1, 2, 3], [4, 5, 6]],
+    [[-1, 1], [10, -10]],
+    [[0, 0], [0, 0]],
+    [[1, 2], [3]]
+]
+
+test_col_sums=[
+    [[1, 2, 3], [4, 5, 6]],
+    [[-1, 1], [10, -10]], 
+    [[0, 0], [0, 0]],
+    [[1, 2], [3]]
+]
+print("TRANSPOSE")
+testing(transpose,test_transpose)
+print()
+print("ROW_SUMS")
+testing(row_sums,test_row_sums)
+print()
+print("COL_SUMS")
+testing(col_sums,test_col_sums)
